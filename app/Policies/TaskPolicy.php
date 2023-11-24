aa2bba2295fa3237a3f748ec5d59968ed6ec26c0
nkmsk <144405987+nkmsk@users.noreply.github.com>
2023-11-24 11:02:56 +0900

    change https

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -5,10 +5,16 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\Task;
 
+use Illuminate\Auth\Access\HandlesAuthorization;
+
 class TaskPolicy
 {
+    use HandlesAuthorization;
+
     /**
      * Create a new policy instance.
+     *
+     * @return void
      */
     public function __construct()
     {
@@ -17,7 +23,7 @@ class TaskPolicy
 
     /**
      * 指定されたユーザーのタスクのとき削除可能
-     * 
+     *
      * @param User $user
      * @param Task $task
      * @return bool
